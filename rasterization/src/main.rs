e38368d935b8936df65393eb12db3e6838d6d8d3
Raimund Krämer <kraemer_raimund@web.de>
2023-02-12 18:56:39 +0100

    Create window with a white image
    This prepares the technical infrastructure for windowing and displaying
    the rendered image. We will be able to later manipulate the pixel data
    to see the result of our rendering algorithms.

--- rasterization/src/main.rs
+++ rasterization/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use show_image::{ImageView, ImageInfo, create_window};
+
+#[show_image::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pixel_data = &[255; 1024 * 1024 * 3];
+    
+    let image = ImageView::new(ImageInfo::rgb8(1024, 1024), pixel_data);
+
+    let window = create_window("Rendered Image", Default::default())?;
+    window.set_image("image", image)?;
+
+    std::io::stdin().read_line(&mut String::new()).unwrap();
+    
+    Ok(())
 }
