699b853ef152d1ec649e13aa76400dbae58d8737
Raimund Krämer <kraemer_raimund@web.de>
2023-02-13 23:03:06 +0100

    Fit window to image size

--- rasterization/src/main.rs
+++ rasterization/src/main.rs
@@ -1,12 +1,15 @@
-use show_image::{ImageView, ImageInfo, create_window};
+use show_image::{create_window, ImageInfo, ImageView, WindowOptions};
 
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pixel_data = &[255; 1024 * 1024 * 3];
-    
-    let image = ImageView::new(ImageInfo::rgb8(1024, 1024), pixel_data);
 
-    let window = create_window("Rendered Image", Default::default())?;
+    let image = ImageView::new(ImageInfo::rgb8(512, 512), pixel_data);
+
+    let window = create_window(
+        "Rendered Image",
+        WindowOptions::new().set_size([512, 512]),
+    )?;
     window.set_image("image", image)?;
     let _ = window.wait_until_destroyed();
 
