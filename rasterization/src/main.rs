66a92604c6c95ab20211ae860cb475bffebe0abd
Raimund Krämer <kraemer_raimund@web.de>
2023-02-14 00:08:49 +0100

    Add RenderBuffer and related types for handling pixel data

--- rasterization/src/main.rs
+++ rasterization/src/main.rs
@@ -1,14 +1,22 @@
 use show_image::{create_window, ImageInfo, ImageView, WindowOptions};
 
+use crate::render_buffer::RenderBuffer;
+
+mod render_buffer;
+
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pixel_data = &[255; 1024 * 1024 * 3];
+    let white_image = RenderBuffer::white(512, 512);
+    let pixel_data = &white_image.raw_pixel_data();
 
-    let image = ImageView::new(ImageInfo::rgb8(512, 512), pixel_data);
+    let image = ImageView::new(
+        ImageInfo::rgba8(white_image.width.into(), white_image.height.into()),
+        pixel_data,
+    );
 
     let window = create_window(
         "Rendered Image",
-        WindowOptions::new().set_size([512, 512]),
+        WindowOptions::new().set_size([white_image.width.into(), white_image.height.into()]),
     )?;
     window.set_image("image", image)?;
     let _ = window.wait_until_destroyed();
---
+++ rasterization/src/render_buffer.rs
@@ -0,0 +1,59 @@
+#[derive(Debug, Clone)]
+pub struct RenderBuffer {
+    pub width: u16,
+    pub height: u16,
+    pixels: Vec<Pixel>,
+}
+
+impl RenderBuffer {
+    pub fn new(width: u16, height: u16) -> Self {
+        let n_bytes = usize::from(width) * usize::from(height);
+        Self {
+            width,
+            height,
+            pixels: vec![TRANSPARENT; n_bytes],
+        }
+    }
+
+    pub fn white(width: u16, height: u16) -> RenderBuffer {
+        let mut buffer = RenderBuffer::new(width, height);
+        let n_bytes = usize::from(width) * usize::from(height);
+        for i in 0..n_bytes {
+            buffer.pixels[usize::from(i)] = WHITE
+        }
+        buffer
+    }
+
+    pub fn raw_pixel_data(&self) -> Vec<u8> {
+        let n_bytes = usize::from(self.width) * usize::from(self.height) * 4;
+        let mut pixel_data = vec![0u8; n_bytes];
+        for i in 0..self.pixels.len() {
+            pixel_data[i * 4 + 0] = self.pixels[i].r;
+            pixel_data[i * 4 + 1] = self.pixels[i].g;
+            pixel_data[i * 4 + 2] = self.pixels[i].b;
+            pixel_data[i * 4 + 3] = self.pixels[i].a;
+        }
+        pixel_data
+    }
+}
+
+const WHITE: Pixel = Pixel {
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 255,
+};
+const TRANSPARENT: Pixel = Pixel {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 0,
+};
+
+#[derive(Debug, Copy, Clone)]
+pub struct Pixel {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
